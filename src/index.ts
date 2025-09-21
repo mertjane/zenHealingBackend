c73ab76c9f3e8aea8a2bfa5737bc6063cc2c6bfd
unknown <mck0391@gmail.com>
2025-09-21 08:00:28 +0100

    pt-gateway commit v3

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: process.env.CLIENT_URL }));
+app.use(cors({
+  origin: process.env.CLIENT_URL,       // must exactly match your Cloudflare URL
+  methods: ["GET", "POST"],
+  credentials: true           // if you send cookies, optional otherwise
+}));
 app.use(express.json());
 
 // Routes
