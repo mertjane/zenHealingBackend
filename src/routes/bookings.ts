61524edfa3b502593df021b51a473376b227dd79
unknown <mck0391@gmail.com>
2025-09-26 12:15:13 +0100

    Booking Update v0.1.1

--- src/routes/bookings.ts
+++ src/routes/bookings.ts
@@ -1,5 +1,5 @@
 import { Router, Request, Response } from "express";
-import { v4 as uuidv4 } from "uuid";
+
 import { pool } from "../utils/db";
 import { Booking } from "../types/Booking";
 import { sendEmail } from "../utils/sendEmail";
@@ -47,6 +47,8 @@ router.post("/", async (req: Request, res: Response) => {
     if ((existing as any[]).length > 0) {
       return res.status(400).json({ error: "Slot already booked" });
     }
+
+    const { v4: uuidv4 } = await import("uuid");
 
     const id = uuidv4();
 
